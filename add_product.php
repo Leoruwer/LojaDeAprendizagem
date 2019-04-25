ef379e52c781f6228983a64b30a545f30de22b42
Leonardo Ruwer <leonardo.ruwer@gmail.com>
2019-04-25 10:47:16 -0300

    Update add_product.php

--- add_product.php
+++ add_product.php
@@ -14,7 +14,7 @@ if(array_key_exists('used', $_POST)) {
 if(insertProduct($connect, $name, $price, $description, $category_id, $used)) {	?>
 	<p class="text-success"><?=$name; ?> no preço: <?= $price; ?>, foi adicionado com sucesso!</p>
 <?php } else {
-		$errorMsg = mysqli_error($connect);
+	$errorMsg = mysqli_error($connect);
 ?>
 	<p class="text-danger">Produto: <?= $name; ?>, não foi adicionado: <?= $errorMsg?></p>
 <?php
